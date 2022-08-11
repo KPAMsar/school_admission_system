5d196d687af74830c9f68d5e7fba4f63f66cf120
unknown <kpamsarshija@gmail.com>
2022-08-11 20:23:51 +0100

    worked on welcome page

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -127,7 +127,93 @@
     </div>
 
 
-    @yield('content')
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Content Wrapper. Contains page content -->
+<div class=" ">
+    <!-- Content Header (Page header) -->
+    <section class="content-header" style="padding-top:5rem;">
+        <div class="container card">
+            <div class="header">
+                <h1 style="text-align:center;"><strong>To start your Application process</strong> With Application<br>
+                    <!-- <small>To stand a chance of gaining admission into our school, start the application process by filling the form below.</small> -->
+
+                </h1>
+                <br>
+
+            </div>
+            <div class="">
+                <div class="container">
+                    <div class="container">
+                        <div class="row clearfix">
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                <div class="">
+                                    <div style="text-align:center;">
+                                        @csrf
+
+                                        <div class="body">
+                                            <div class="row">
+                                                1.Proceed to Admissions section and select your application program
+                                            </div><br>
+                                            <div class="row">
+                                                1.Proceed to Admissions section and select your application program
+                                            </div><br>
+                                            <div class="row">
+                                                1.Proceed to Admissions section and select your application program
+                                            </div><br>
+                                            <div class="row">
+                                                1.Proceed to Admissions section and select your application program
+                                            </div><br>
+                                            <div class="row">
+                                                1.Proceed to Admissions section and select your application program
+                                            </div><br>
+                                            <div class="row">
+                                                1.Proceed to Admissions section and select your application program
+                                            </div><br>
+                                            <div class="row">
+                                                1.Proceed to Admissions section and select your application program
+                                            </div><br>
+                                            <div class="row">
+                                                1.Proceed to Admissions section and select your application program
+                                            </div><br>
+                                            <div class="row">
+                                                1.Proceed to Admissions section and select your application program
+                                            </div><br>
+                                            
+
+                                           
+                                            
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+    </section>
+
+
+</div>
+<!-- /.content-wrapper -->
+
+
+
+
+
+
+
 
 
 
